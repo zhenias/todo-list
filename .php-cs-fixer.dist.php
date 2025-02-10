53979a5046a14102fe9b77b39005aadd906b13a4
zhenias <jekasamarash@gmail.com>
2025-02-10 17:23:23 +0100

    add structure laravel. And add php cs fixer.

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setFinder(
+        (new Finder())
+            ->ignoreDotFiles(false)
+            ->ignoreVCSIgnored(true)
+            ->exclude(['dev-tools/phpstan', 'tests/Fixtures'])
+            ->in(__DIR__)
+            ->append([__DIR__.'/php-cs-fixer'])
+    )
+;
